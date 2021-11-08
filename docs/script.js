b8117a6e42362c1a55a2da22686c555caa484a4e
doramatadora <hello@doramilitaru.com>
2021-11-08 08:39:08 +0000

    set twitter img explicitly
    update
    
    update
    
    update
    
    update
    
    update
    
    update
    
    update

--- docs/script.js
+++ docs/script.js
@@ -1,4 +1,4 @@
-const DEMO_URL="https://endlessly-enjoyed-martin.edgecompute.app";
+const DEMO_URL="https://social-cards-demo.edgecompute.app";
 const TWEET_URL =
   "https://twitter.com/intent/tweet?text="+encodeURIComponent("Social sharing cards generated on Fastly's Compute@Edge, how neat!") + "&url=";
 const preview = document.getElementById("preview");
@@ -7,6 +7,7 @@ const title = document.getElementById("title");
 const huerot = document.getElementById("huerot");
 const tweet = document.getElementById("tweet");
 const meta = document.querySelector('meta[property="og:image"]');
+const twimg = document.querySelector('meta[property="twitter:image"]');
 
 // By Trey Huffine (https://gist.github.com/treyhuffine/2ced8b8c503e5246e2fd258ddbd21b8c#file-debounce-js)
 const debounce = (func, wait) => {
@@ -22,10 +23,11 @@ const debounce = (func, wait) => {
 };
 
 const updateSocialCard = debounce(function () {
-  const qs = new URLSearchParams(new FormData(formEl)).toString();
+  const qs = "title=" + title.value + "&huerot=" + huerot.value;
   const url = DEMO_URL + "/card/?" + qs;
   preview.src = url;
   meta.setAttribute("content", url);
+  twimg.setAttribute("content", url);
   tweet.setAttribute("href", TWEET_URL + encodeURIComponent(DEMO_URL + "/?" + qs));
 }, 250);
 
@@ -34,7 +36,7 @@ huerot.addEventListener("change", updateSocialCard);
 
 const urlParams = new URLSearchParams(window.location.search);
 
-title.value = urlParams.get("title") || "Edit the social card title";
+title.value = decodeURIComponent(urlParams.get("title")) || "Edit the social card title";
 huerot.value = urlParams.get("huerot") || 0;
 
 updateSocialCard();
